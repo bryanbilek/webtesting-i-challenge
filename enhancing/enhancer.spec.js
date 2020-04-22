d088b482b22a018360b846b5046ff25346fa2318
bryanbilek <you@example.com>
2020-04-22 17:55:52 -0500

    setup the functions in enhancer, so far repair works in test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const {succeed, fail , repair , get} = require('./enhancer.js');
+
+//test items
+const itemOne = { 
+    durability: 50,
+    enhancement: 20
+}
+const itemTwo = { 
+    durability: 50,
+    enhancement: 19
+}
+const itemThree = { 
+    durability: 50,
+    enhancement: 14
+}
+const itemFour = { 
+    durability: 50,
+    enhancement: 17
+}
+
+describe("repair", () => {
+  it("should output a durability of 100 for any item", () => {
+    expect(repair(itemOne.durability)).toBe(100);
+    expect(repair(itemTwo.durability)).toBe(100);
+    expect(repair(itemThree.durability)).toBe(100);
+  });
+});
