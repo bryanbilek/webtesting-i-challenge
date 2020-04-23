80e70a0c74a6e71366c9308b080bc60995ca62ea
bryanbilek <you@example.com>
2020-04-23 15:41:45 -0500

    had the right concept/algos but needed to update the spread op to reflect changes to the item instead of making a new one

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -7,7 +7,7 @@ module.exports = {
 
 function succeed(item) {
   if (item.enhancement < 20) {
-   item.enhancement += 1;
+  return {...item, enhancement: item.enhancement += 1};
   } else {
     return {...item};
   }
@@ -15,18 +15,18 @@ function succeed(item) {
 
 function fail(item) {
   if (item.enhancement < 15) {
-    item.durability -= 5;
+   return {...item, durability: item.durability -= 5};
   } else if (item.enhancement > 16) {
-    item.enhancement -= 1;
+  return {...item, enhancement: item.enhancement -= 1};
   } else if(item.enhancement >= 15) {
-    item.durability -= 10;
+  return {...item, durability: item.durability -= 10};
   } else {
     return {...item}
   }
 }
 
 function repair(item) {
-  return { ...item }, item.durability = 100;
+  return { ...item, durability: item.durability = 100};
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -25,42 +25,42 @@ const itemFive = {
 
 describe("succeed", () => {
     it("item enhancement increases by 1 when i.enh < 20", () => {
-        succeed(itemTwo);
-        expect((itemTwo.enhancement)).toBe(20);
+       const succeededItem = succeed(itemTwo);
+        expect((succeededItem.enhancement)).toBe(20);
     });
 
     it("item enhancement remains 20 if i.enh = 20", () => {
-        succeed(itemOne);
-        expect((itemOne.enhancement)).toBe(20);
+       const succeededItem = succeed(itemOne);
+        expect((succeededItem.enhancement)).toBe(20);
     });
 
     it("item durability = unchanged from suceed", () => {
-        succeed(itemOne);
-        expect((itemOne.durability)).toBe(50);
+        const succeededItem = succeed(itemOne);
+        expect((succeededItem.durability)).toBe(50);
     });
 });
 
 describe("fail", () => {
     it("i.enh < 15 then i.dur decreases by 5", () => {
-        fail(itemThree);
-        expect((itemThree.durability)).toBe(45);
+        const failedItem = fail(itemThree);
+        expect((failedItem.durability)).toBe(45);
     });
 
     it("i.enh >= 15 then i.dur decreases by 10", () => {
-        fail(itemFour);
-        expect((itemFour.durability)).toBe(40);
+       const failedItem = fail(itemFour);
+        expect((failedItem.durability)).toBe(40);
     });
 
     it("i.enh > 16 then i.enh decreases by 1", () => {
-        fail(itemFive);
-        expect((itemFive.enhancement)).toBe(16);
+       const failedItem = fail(itemFive);
+        expect((failedItem.enhancement)).toBe(16);
     });
 });
 
 describe("repair", () => {
     it("should output a durability of 100 for any item", () => {
-      expect(repair(itemOne.durability)).toBe(100);
-      expect(repair(itemTwo.durability)).toBe(100);
-      expect(repair(itemThree.durability)).toBe(100);
+      expect(repair(itemOne).durability).toBe(100);
+      expect(repair(itemTwo).durability).toBe(100)
+      expect(repair(itemThree).durability).toBe(100);
     });
 });
